c8bb01a17faa88b4a01a5c9f32fb78803547c1cc
Andrew Noskov <anoskov@ccsteam.ru>
2016-09-05 17:24:29 +0300

    init project

---
+++ README.md
@@ -0,0 +1,28 @@
+# API Controller
+
+Base API controller for Phoenix.
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+* Add `api_controller` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [{:api_controller, "~> 0.1"]
+end
+```
+
+* Ensure `api_controller`is started before your application:
+
+```elixir
+def application do
+  [applications: [:api_controller]]
+end
+```
+
+## Usage
+
+
+## Configuration
---
+++ mix.exs
@@ -0,0 +1,39 @@
+defmodule ApiController.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :api_controller,
+     version: "0.1.0",
+     elixir: "~> 1.3",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps(),
+     aliases: aliases(),
+     description: description(),
+     package: package()]
+  end
+
+  def application do
+    [applications: []]
+  end
+
+  defp deps do
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp aliases do
+    ["test": ["test --no-start"]]
+  end
+
+  defp description do
+    "Base API Controller for Phoenix"
+  end
+
+  defp package do
+    [name: :api_controller,
+     files: ["lib", "mix.exs"],
+     maintainers: ["Andrey Noskov"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/anoskov/ex_api_controller"}]
+  end
+end
